fafc06d3cbad3d73816699c3147af09542f2e031
c6312900 <c6312900@gmail.com>
2017-05-11 17:56:45 +0800

    1060511 ch02-04

---
+++ Mod02_01/Controllers/OperaController.cs
@@ -0,0 +1,39 @@
+﻿using Mod02_01.DAL;
+using Mod02_01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mod02_01.Controllers
+{
+    public class OperaController : Controller
+    {
+        // GET: Opera
+        // GET: Opera/Index?operaId=111&title=xx&year=1923&composer=John
+        //public ActionResult Index(Opera opera)
+        //{
+        //    //var test = ModelState.IsValid;
+        //    //Opera o = new Opera()
+        //    //{
+        //    //    OperaID = opera.OperaID,
+        //    //    Title = opera.Title,
+        //    //    Year = opera.Year,
+        //    //    Composer = opera.Composer
+        //    //};
+
+        //    return View(opera);
+        //}
+
+        // Lab2_4
+        // GET: Opera/Index
+        public ActionResult Index()
+        {
+            OperaContext context = new OperaContext();
+
+            return View(context.Operas.ToList());
+        }
+
+    }
+}
